fa8eecef7ae7ee539a5989fddafcc4f1a768f18d
loreprocks <lorenajrc@gmail.com>
2022-09-01 16:43:07 -0500

    configuration of eslint & prettier

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,13 +1,12 @@
-import styles from '../../styles/Button.module.css'
-const Button = ({children, onClick}) => {
-    return (
-        <>
-            <button className={styles.btn} onClick={onClick}>
-                {children}
-            </button>
-        </>
-    )
-
+import styles from "../../styles/Button.module.css"
+const Button = ({ children, onClick }) => {
+  return (
+    <>
+      <button className={styles.btn} onClick={onClick}>
+        {children}
+      </button>
+    </>
+  )
 }
 
-export default Button;
+export default Button
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import Button from '../components/Button'
-import GitHub from '../components/Icons/GitHub'
-import styles from '../styles/Home.module.css'
-import {loginWithGitHub, onAuthStateChanged } from '../firebase/client'
+import { useEffect, useState } from "react"
+import Head from "next/head"
+import Image from "next/image"
+import Button from "../components/Button"
+import GitHub from "../components/Icons/GitHub"
+import styles from "../styles/Home.module.css"
+import { loginWithGitHub, onAuthStateChanged } from "../firebase/client"
 
 export default function Home() {
-
   const [user, setUser] = useState(undefined)
 
   useEffect(() => {
@@ -15,12 +14,14 @@ export default function Home() {
   }, [])
 
   const handleClick = () => {
-    loginWithGitHub().then(user => {
-      console.log(user)
-      setUser(user)
-    }).catch(err => {
-      console.log(err)
-    })
+    loginWithGitHub()
+      .then((user) => {
+        console.log(user)
+        setUser(user)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
@@ -29,34 +30,36 @@ export default function Home() {
         <title>devter 🐦</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <div className={styles.container}>
         <main className={styles.main}>
-            <section className={styles.wrap}>
-                <Image src='/devter-logo.png' layout='fixed' width={180} height={180}></Image>
-                <h1 className={styles.title}>Devter</h1>
-                <h2 className={styles.subtitle}>
-                    <span>Talk about development with developers</span> 👩🏻‍💻👨🏻‍💻</h2>
+          <section className={styles.wrap}>
+            <Image
+              src="/devter-logo.png"
+              layout="fixed"
+              width={180}
+              height={180}
+            ></Image>
+            <h1 className={styles.title}>Devter</h1>
+            <h2 className={styles.subtitle}>
+              <span>Talk about development with developers</span> 👩🏻‍💻👨🏻‍💻
+            </h2>
+            <div>
+              {user === null && (
+                <Button onClick={handleClick}>
+                  <GitHub width={24} height={24} fill="#fff" />
+                  Login with GitHub
+                </Button>
+              )}
+              {user && user.avatar && (
                 <div>
-                  {
-                    user === null &&
-                      <Button onClick={handleClick}>
-                          <GitHub width={24} height={24} fill="#fff"/>
-                          Login with GitHub
-                      </Button> 
-                  }
-                  {
-                    user && user.avatar && 
-                    <div>
-                      <img src={user.avatar} alt='avatar'></img>
-                      <h3>{user.username}</h3>
-                      </div>
-                  }
+                  <img src={user.avatar} alt="avatar"></img>
+                  <h3>{user.username}</h3>
                 </div>
-            </section>
+              )}
+            </div>
+          </section>
         </main>
       </div>
-
     </>
   )
 }
