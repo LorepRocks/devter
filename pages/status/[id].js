85341f993c23babd109bc6ff81bcff7c130ddc98
loreprocks <lorenajrc@gmail.com>
2022-09-19 21:34:11 -0500

    configuring real time updates with firebase and setting env files

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -2,6 +2,10 @@ import Devit from "components/Devit"
 
 export default function DevitPage(props) {
   console.log("received props", props)
+  if (!props.createAt) {
+    return
+  }
+
   return <Devit {...props} />
 }
 
@@ -10,6 +14,7 @@ export async function getServerSideProps(context) {
   const { id } = params
 
   const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+  console.log("___apiResponse", apiResponse)
   if (apiResponse.ok) {
     const props = await apiResponse.json()
     return { props }
